303e7f04e43a94167955e56ef5744996e95751c4
QuantumBen <omosabenedict@gmail.com>
2026-09-02 01:28:53 -0700

    Fetch data from the backend server we setup

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
-import Jobs from '../jobs.json'
+import { useState, useEffect, use } from 'react'
 import JobListing from './Joblisting'
 
 const JobListings = ({ isHome = false }) => {
-    const jobListings = isHome ? Jobs.slice(0, 3) : Jobs // Get the first 3 jobs if isHome is true, otherwise get all jobs
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchJobs = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/jobs');
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch jobs');
+                }
+
+                const data = await response.json();
+                setJobs(isHome ? data.slice(0, 3) : data);
+            } catch (error) {
+                console.error('Error fetching jobs:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchJobs();
+    }, [isHome]);
+
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                   { isHome ? 'Latest  Jobs' : 'Browse Jobs'}
+                    {isHome ? 'Latest  Jobs' : 'Browse Jobs'}
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {jobListings.map((job) => (
-                       <JobListing  key={job.id} job={job} />
-                    ))}
+                    { loading ? <h2>Loading ...</h2> : jobs.map((job) => (
+                        <JobListing key={job.id} job={job} />
+                    )) }
 
                 </div>
             </div>
