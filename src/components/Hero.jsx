342f149eb0120dcf8d5e619ef2f89aced6a202ec
QuantumBen <omosabenedict@gmail.com>
2026-09-01 23:58:25 -0700

    Added a default props here

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const Hero = (props) => {
+const Hero = ({
+  title = 'Become a React Dev',
+  subtitle = 'Find the React job',
+}) => {
   return (
       <section className="bg-indigo-700 py-20 mb-4">
         <div
@@ -10,10 +13,10 @@ const Hero = (props) => {
             <h1
               className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
             >
-              {props.title}
+              {title}
             </h1>
             <p className="my-4 text-xl text-white">
-                {props.subtitle}
+                {subtitle}
             </p>
           </div>
         </div>
