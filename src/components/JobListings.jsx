8a21b5cf0d18581b528fd624cf20b376f13dc19f
QuantumBen <omosabenedict@gmail.com>
2026-09-02 00:57:29 -0700

    using props and tenary operator

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import Jobs from '../jobs.json'
 import JobListing from './Joblisting'
 
-const JobListings = () => {
-    const recentJobs = Jobs.slice(0, 3) // Get the first 3 jobs from the JSON file
+const JobListings = ({ isHome = false }) => {
+    const jobListings = isHome ? Jobs.slice(0, 3) : Jobs // Get the first 3 jobs if isHome is true, otherwise get all jobs
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    Browse Jobs
+                   { isHome ? 'Latest  Jobs' : 'Browse Jobs'}
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {recentJobs.map((job) => (
+                    {jobListings.map((job) => (
                        <JobListing  key={job.id} job={job} />
                     ))}
 
