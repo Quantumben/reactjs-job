b7fcfab6f25682dd69119095f172365fc0bcb6eb
QuantumBen <omosabenedict@gmail.com>
2026-09-02 00:36:23 -0700

    little cleanup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/images/logo.png'
 
+
 const Navbar = () => {
     return (
             <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -11,7 +12,7 @@ const Navbar = () => {
                             className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
                         >
                             {/* <!-- Logo --> */}
-                            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+                            <Link className="flex flex-shrink-0 items-center mr-4" to="/">
                                 <img
                                     className="h-10 w-auto"
                                     src={logo}
@@ -20,7 +21,7 @@ const Navbar = () => {
                                 <span className="hidden md:block text-white text-2xl font-bold ml-2"
                                 >React Jobs</span
                                 >
-                            </a>
+                            </Link>
                             <div className="md:ml-auto">
                                 <div className="flex space-x-2">
                                     <Link
