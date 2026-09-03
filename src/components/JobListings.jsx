3a935e23efa8d965bf7de5f22da744fc7911f526
QuantumBen <omosabenedict@gmail.com>
2026-09-02 23:42:23 -0700

    Added proxy and used proxy to fetch API

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -9,7 +9,7 @@ const JobListings = ({ isHome = false }) => {
     useEffect(() => {
         const fetchJobs = async () => {
             try {
-                const response = await fetch('http://localhost:8000/jobs');
+                const response = await fetch('/api/jobs');
 
                 if (!response.ok) {
                     throw new Error('Failed to fetch jobs');
